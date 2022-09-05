8755631eb16ee49a009a0aac2d5a4f203be573ad
igor-suzart <igorsuzart@protonmail.com>
2022-09-05 17:52:40 -0300

    pequenas correções

--- lib/views/welcome/signup.dart
+++ lib/views/welcome/signup.dart
@@ -33,7 +33,6 @@ class Form extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
-      // ignore: prefer_const_literals_to_create_immutables
       children: <Widget>[
         const Text(
           'Sign up',
@@ -53,120 +52,121 @@ class Form extends StatelessWidget {
         ),
         SizedBox(height: 25.0),
         const SizedBox(
-          width: 429,
+          width: 350,
           child: Text(
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt non porttitor hac bibendum nulla dolor. Adipiscing pulvinar ac aliquet ornare. Nulla proin arcu, scelerisque non porttitor lorem.'),
         ),
-        // ignore: prefer_const_constructors
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
         ),
         Container(
           height: 500,
-          width: 458,
+          width: 350,
           child: SingleChildScrollView(
             child: Column(
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // ignore: prefer_const_literals_to_create_immutables
+
               children: <Widget>[
-                Row(
-                  children: const <Widget>[
-                    SizedBox(
-                      width: 220.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Name",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
+                Container(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    children: const <Widget>[
+                      SizedBox(
+                        width: 167.0,
+                        child: TextField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Name",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: TextStyle(fontSize: 15.0),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 18,
-                    ),
-                    // ignore: prefer_const_constructors
-                    SizedBox(
-                      width: 220.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Last Name",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
+                      SizedBox(
+                        width: 16,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 167.0,
+                        child: TextField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Last Name",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 20),
                 Column(
                   children: const <Widget>[
-                    SizedBox(
-                      width: 458.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Email",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: TextField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Email",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: TextStyle(fontSize: 15.0),
+                        ),
                       ),
                     ),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 458.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Birthday",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
+                    Padding(
+                        padding: EdgeInsets.only(bottom: 16),
+                        child: SizedBox(
+                          width: 350.0,
+                          child: TextField(
+                            // ignore: unnecessary_const
+                            decoration: const InputDecoration(
+                                hintText: "Birthday",
+                                border: OutlineInputBorder(),
+                                isDense: true,
+                                contentPadding: EdgeInsets.all(15)),
+
+                            style: TextStyle(fontSize: 15.0),
+                          ),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.only(bottom: 16),
+                        child: SizedBox(
+                          width: 350.0,
+                          child: TextField(
+                            // ignore: unnecessary_const
+                            decoration: const InputDecoration(
+                                hintText: "Password",
+                                border: OutlineInputBorder(),
+                                isDense: true,
+                                contentPadding: EdgeInsets.all(15)),
+
+                            style: TextStyle(fontSize: 15.0),
+                          ),
+                        )),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: TextField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Repeat Password",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: TextStyle(fontSize: 15.0),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 458.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Password",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: 458.0,
-                      // ignore: prefer_const_constructors
-                      child: TextField(
-                        // ignore: unnecessary_const
-                        decoration: const InputDecoration(
-                            hintText: "Repeat Password",
-                            border: OutlineInputBorder(),
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(15)),
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ),
+                    )
                   ],
                 )
               ],
