b77aadb91d7c66d7eb5a8796128555f2b58c7e60
lopix320 <gabriel.lp2008@gmail.com>
2022-09-05 08:42:03 -0300

    começo da página de login

--- lib/views/welcome/signup.dart
+++ lib/views/welcome/signup.dart
@@ -26,8 +26,28 @@ class Form extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const <Widget>[Text('teste maroto')]);
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text('Sign up',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48,
+                fontFamily: 'Heebo',
+                height: 0.9)),
+        const Text('Welcome!',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: Color(0xff30AAD8),
+                fontSize: 15,
+                fontFamily: 'Heebo')),
+        const SizedBox(
+          width: 350,
+          child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt non porttitor hac bibendum nulla dolor. Adipiscing pulvinar ac aliquet ornare. Nulla proin arcu, scelerisque non porttitor lorem.'),
+        ),
+        Padding(padding: const EdgeInsets.fromLTRB(0, 24, 0, 0)),
+      ],
+    );
   }
 }
