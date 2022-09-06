3f82bb7f7d001046f4c94994596a84c204931ce6
lopix320 <gabriel.lp2008@gmail.com>
2022-09-06 14:05:09 -0300

    adicionando button e dropdownmenu e endireitando layout

--- lib/views/welcome/signup.dart
+++ lib/views/welcome/signup.dart
@@ -1,4 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fxv_ide/modals/country.dart';
+// import 'package:http/http.dart' as http;
+
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
+// Future<Album> fetchAlbum() async {
+//   final response = await http.get(
+//       Uri.parse('https://servicodados.ibge.gov.br/api/v1/paises/{paises} '));
+
+//   if (response.statusCode == 200) {
+//     // print(response.body);
+
+//     return Album.fromJson(jsonDecode(response.body[0]));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -12,13 +33,14 @@ class SignUp extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(36.0)),
           elevation: 15,
           child: Container(
-            padding: const EdgeInsets.fromLTRB(2, 40, 2, 40),
-            child: SingleChildScrollView(
-              child: Container(
-                  padding: const EdgeInsets.fromLTRB(64, 25, 64, 60),
-                  child: const Form()),
-            ),
-          ),
+              // height: 600,
+              padding: const EdgeInsets.fromLTRB(45, 70, 30, 40),
+              // child: SingleChildScrollView(
+              // child: Container(
+              // padding: const EdgeInsets.fromLTRB(45, 25, 45, 10),
+              child: const Form()),
+          // ),
+          // ),
         )
       ],
     );
@@ -50,30 +72,29 @@ class Form extends StatelessWidget {
               fontSize: 15,
               fontFamily: 'Heebo'),
         ),
-        SizedBox(height: 25.0),
+        const SizedBox(height: 25.0),
         const SizedBox(
           width: 350,
           child: Text(
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt non porttitor hac bibendum nulla dolor. Adipiscing pulvinar ac aliquet ornare. Nulla proin arcu, scelerisque non porttitor lorem.'),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
         ),
-        Container(
-          height: 500,
-          width: 350,
+        SizedBox(
+          height: 245,
+          width: 365,
           child: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
             child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.only(bottom: 16),
+                  padding: const EdgeInsets.only(bottom: 16),
                   child: Row(
-                    children: const <Widget>[
+                    children: <Widget>[
                       SizedBox(
                         width: 167.0,
-                        child: TextField(
+                        child: TextFormField(
                           // ignore: unnecessary_const
                           decoration: const InputDecoration(
                               hintText: "Name",
@@ -81,15 +102,15 @@ class Form extends StatelessWidget {
                               isDense: true,
                               contentPadding: EdgeInsets.all(15)),
 
-                          style: TextStyle(fontSize: 15.0),
+                          style: const TextStyle(fontSize: 15.0),
                         ),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         width: 16,
                       ),
                       SizedBox(
                         width: 167.0,
-                        child: TextField(
+                        child: TextFormField(
                           // ignore: unnecessary_const
                           decoration: const InputDecoration(
                               hintText: "Last Name",
@@ -97,19 +118,19 @@ class Form extends StatelessWidget {
                               isDense: true,
                               contentPadding: EdgeInsets.all(15)),
 
-                          style: TextStyle(fontSize: 15.0),
+                          style: const TextStyle(fontSize: 15.0),
                         ),
                       ),
                     ],
                   ),
                 ),
                 Column(
-                  children: const <Widget>[
+                  children: <Widget>[
                     Padding(
-                      padding: EdgeInsets.only(bottom: 16),
+                      padding: const EdgeInsets.only(bottom: 16),
                       child: SizedBox(
                         width: 350.0,
-                        child: TextField(
+                        child: TextFormField(
                           // ignore: unnecessary_const
                           decoration: const InputDecoration(
                               hintText: "Email",
@@ -117,45 +138,47 @@ class Form extends StatelessWidget {
                               isDense: true,
                               contentPadding: EdgeInsets.all(15)),
 
-                          style: TextStyle(fontSize: 15.0),
+                          style: const TextStyle(fontSize: 15.0),
                         ),
                       ),
                     ),
                     Padding(
-                        padding: EdgeInsets.only(bottom: 16),
-                        child: SizedBox(
-                          width: 350.0,
-                          child: TextField(
-                            // ignore: unnecessary_const
-                            decoration: const InputDecoration(
-                                hintText: "Birthday",
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(15)),
-
-                            style: TextStyle(fontSize: 15.0),
-                          ),
-                        )),
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 16),
-                        child: SizedBox(
-                          width: 350.0,
-                          child: TextField(
-                            // ignore: unnecessary_const
-                            decoration: const InputDecoration(
-                                hintText: "Password",
-                                border: OutlineInputBorder(),
-                                isDense: true,
-                                contentPadding: EdgeInsets.all(15)),
-
-                            style: TextStyle(fontSize: 15.0),
-                          ),
-                        )),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 16),
+                      padding: const EdgeInsets.only(bottom: 16),
                       child: SizedBox(
                         width: 350.0,
-                        child: TextField(
+                        child: TextFormField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Birthday",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: const TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: TextFormField(
+                          // ignore: unnecessary_const
+                          decoration: const InputDecoration(
+                              hintText: "Password",
+                              border: OutlineInputBorder(),
+                              isDense: true,
+                              contentPadding: EdgeInsets.all(15)),
+
+                          style: const TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: TextFormField(
                           // ignore: unnecessary_const
                           decoration: const InputDecoration(
                               hintText: "Repeat Password",
@@ -163,7 +186,49 @@ class Form extends StatelessWidget {
                               isDense: true,
                               contentPadding: EdgeInsets.all(15)),
 
-                          style: TextStyle(fontSize: 15.0),
+                          style: const TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            hintText: 'Country',
+                            border: OutlineInputBorder(),
+                            isDense: true,
+                            contentPadding: EdgeInsets.all(15),
+                          ),
+                          items: list.map<DropdownMenuItem<String>>(
+                            (String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            },
+                          ).toList(),
+                          onChanged: ((value) => {}),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: SizedBox(
+                        width: 350.0,
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          // ignore: sort_child_properties_last
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(color: Color(0xff30AAD8)),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xffdbdbdb),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 20),
+                          ),
                         ),
                       ),
                     )
