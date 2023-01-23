2f8a263971ba9a057a3a43c78a36e65b084f54f4
dann-lumi <dannilu.fag@gmail.com>
2023-01-23 18:33:35 -0300

    Container do Publish ajustado na medida e inserido o botao com a imagem do usuario sem ajustes.

--- lib/views/feed/feed_publish.dart
+++ lib/views/feed/feed_publish.dart
@@ -1,4 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:fxv_ide/models/user_models.dart';
+
 
 class FeedPublish extends StatefulWidget{
   @override
@@ -9,16 +13,41 @@ class FeedPublish extends StatefulWidget{
 
 }
 class FeedPublishState extends State<FeedPublish>{
+  String userPhoto = "/uploads/default/avatar-image.jpg";
+  late Uint8List image;
+  int raiseCrop = 0;
+  dynamic filename;
+  late UserModels userData;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      width: 100,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black12,
+        ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Image.network(
+            "http://localhost:4040/getImage?photo=${userPhoto}"),
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(CircleBorder()),
+          padding: MaterialStateProperty.all(EdgeInsets.all(8)),
+          backgroundColor: MaterialStateProperty.all(
+              Color(0xffffffff)), // <-- Button color
+          overlayColor:
+          MaterialStateProperty.resolveWith<Color?>((states) {
+            if (states.contains(MaterialState.pressed))
+              return Color(0xfff4f4f8); // <-- Splash color
+          }),
+        ),
+      ),
+      width: 5000,
       height: 100,
       margin: const EdgeInsets.all(10.0),
-      color: Colors.yellow,
       alignment: Alignment.topCenter,
-
     );
   }
 
